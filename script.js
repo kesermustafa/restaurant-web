098d406cd803d73f33967a572afc3a81a4cffe02
Mustafa Keser <keserzade@gmail.com>
2024-02-04 17:37:14 +0300

    update

--- script.js
+++ script.js
@@ -2,12 +2,9 @@ const searchForm = document.querySelector(".search-form");
 const cartItem = document.querySelector(".cart-items-container");
 const navbar = document.querySelector(".navbar");
 
-
-
 const searchBtn = document.querySelector("#search-btn");
 const cartBtn = document.querySelector("#cart-btn");
 const menuBtn = document.querySelector("#menu-btn");
-
 searchBtn.addEventListener("click", function () {
   searchForm.classList.toggle("active");
   document.addEventListener("click", function (e) {
@@ -20,26 +17,24 @@ searchBtn.addEventListener("click", function () {
   });
 });
 
-
 cartBtn.addEventListener("click", function () {
   cartItem.classList.toggle("active");
   document.addEventListener("click", function (e) {
     if (
-        !e.composedPath().includes(cartBtn) &&
-        !e.composedPath().includes(cartItem)
+      !e.composedPath().includes(cartBtn) &&
+      !e.composedPath().includes(cartItem)
     ) {
       cartItem.classList.remove("active");
     }
   });
 });
 
-
 menuBtn.addEventListener("click", function () {
   navbar.classList.toggle("active");
   document.addEventListener("click", function (e) {
     if (
-        !e.composedPath().includes(menuBtn) &&
-        !e.composedPath().includes(navbar)
+      !e.composedPath().includes(menuBtn) &&
+      !e.composedPath().includes(navbar)
     ) {
       navbar.classList.remove("active");
     }
